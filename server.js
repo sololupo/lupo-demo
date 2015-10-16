5743c16e8608cd242845952be051b8a9b573ab68
sololupo <sololupo.com@gmail.com>
2015-10-16 02:07:02 -0400

    fixing server.js

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 var express = require('express'),
 app = express();
 app.use(express.static('www'));
-app.set('port', process.env.PORT || 5000);
+// extra for html5
+
+app.use("/js", express.static(__dirname + "/www/js"));
+app.use("/img", express.static(__dirname + "/www/img"));
+app.use("/css", express.static(__dirname + "/www/css"));
+app.use("/templates", express.static(__dirname + "/www/templates"));
+
+// end extra
+app.get('/*', function(req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+    res.sendFile(__dirname + '/www/index.html');
+});
+
+app.set('port', process.env.PORT || 8100);
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
